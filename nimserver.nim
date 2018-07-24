00115703aea04b0937640f94e6abf58f46a9850b
shch <shch.pc@gmail.com>
2018-07-24 16:04:36 +0000

    save

--- nimserver.nim
+++ nimserver.nim
@@ -6,18 +6,23 @@ import nimserverpkg/router
 
 proc main() =
 
-    proc index(req: Request) : Future[void] =
-        req.respond(Http200, h1 "Hello")
+    proc index(f: RouteFunc): RouteFunc =
+        return proc(req: Request) : RouteResult =
+            discard req.respond(Http200, h1 "Hello")
+            return RouteResult(request:req)
 
-    proc notfound(r:Request) : Future[void] =
-        r.respond(Http404, "404")
+    proc notfound(f: RouteFunc): RouteFunc =
+        return proc(r:Request) : RouteResult =
+            discard r.respond(Http404, "404")
+            return RouteResult(request:r)
 
-    var indexRoute = get("/", index)
-    var testRoute = get("/{i}", index)
-    var r = newRouter(notfound, indexRoute, testRoute)
+    var indexRoute = get >=> index
     
+    var handler = choose(@[indexRoute])
+    # var testRoute = get("/{i}", index)
+    var r = newRouter(handler, notfound)
     
-    proc cb(req:Request){.async.} =
+    proc cb(req:Request) {.async.} =
         await r.routing(req)
     let server = newAsyncHttpServer(true, true)
     waitfor server.serve(Port(8080), cb)
--- nimserverpkg/router.nim
+++ nimserverpkg/router.nim
@@ -3,39 +3,80 @@ import
     asyncdispatch,
     sequtils
 
+
+# Router Types
 type
-    RouteHandler* = proc (req:Request): Future[void]
-    Route* = ref object of RootObj
-        httpMethod*:        HttpMethod
-        path*:              string
-        handler*:           RouteHandler
+    RouteResult* = ref object of RootObj
+        request*:           Request
+    RouteFunc* = proc (req:Request): RouteResult
+    RouteHandler* = proc (f:RouteFunc): RouteFunc
+    # Route* = ref object of RootObj
+    #     httpMethod*:        HttpMethod
+    #     path*:              string
+    #     handler*:           RouteHandler
     Router* = ref object of RootObj
-        routes:             seq[Route]
+        handler:            RouteHandler
         notFoundHandler:    RouteHandler
 
-proc newRouter*(notFoundHandler: RouteHandler, routes: varargs[Route]): Router =
+let abort*:RouteResult = nil
+
+proc isMatchMethod(isMatch:proc(httpMethod:HttpMethod): bool): RouteHandler =
+    return proc(f:RouteFunc): RouteFunc = 
+        return proc(req:Request): RouteResult =
+            if isMatch req.reqMethod:
+                return f req
+            else:
+                return abort
+
+let get*    = isMatchMethod(proc(m:HttpMethod):bool = m == HttpGet)
+let post*   = isMatchMethod(proc(m:HttpMethod):bool = m == HttpPost)
+
+
+proc chooseFuncs(funcs:seq[RouteFunc]): RouteFunc = 
+    return proc(req: Request): RouteResult =
+        if funcs.len == 0:
+            return nil
+        else:
+            let res = funcs[0] req
+            if res != nil:
+                return res
+            else:
+                let f = chooseFuncs funcs[1..funcs.len-1]
+                return f req
+
+proc choose*(handlers:seq[RouteHandler]): RouteHandler =
+    return proc(f: RouteFunc): RouteFunc =
+        let funcs = handlers.map(proc(h:RouteHandler):RouteFunc = h f)
+        return proc(req: Request): RouteResult =
+            return chooseFuncs(funcs) req
+
+# router
+proc newRouter*(handler: RouteHandler, notFoundHandler: RouteHandler): Router =
     result = Router(
-        routes: @routes,
+        handler: handler,
         notFoundHandler: notFoundHandler
     )
 
-proc routing*(router: Router, req:Request) {.async.} =
-    let filterdByPath       = router.routes.filter(proc (r:Route): bool = r.path == req.url.path )
-    let filterdByMethod     = filterdByPath.filter(proc (r:Route): bool = r.httpMethod == req.reqMethod)
-    if filterdByMethod.len == 0 :
-        await router.notFoundHandler req
-    else :
-        await filterdByMethod[0].handler req
+proc start(req:Request): RouteResult =
+    return RouteResult(request:req)
+    
 
-proc get*(path: string, handler: RouteHandler): Route =
-    return Route(
-        path: path,
-        httpMethod: HttpGet,
-        handler: handler
-    )
-proc post*(path:string, handler: RouteHandler): Route =
-    return Route(
-        path:path,
-        httpMethod: HttpPost,
-        handler: handler
-    )
+proc terminate(req: Request, h: RouteHandler): Future[void] =
+    discard (h start) req
+
+proc `>=>`*(h1,h2: RouteHandler): RouteHandler =
+    return proc(f: RouteFunc): RouteFunc =
+        let f2 = h2 f
+        let f1 = h1 f2
+        return proc(req: Request): RouteResult = 
+            return f1 req
+
+proc routingSync(router: Router, req: Request): Future[void] =
+    let res = (router.handler start) req
+    if res == nil:
+        discard terminate(req, router.notFoundHandler)
+    
+
+proc routing*(router: Router, req:Request) {.async.} =
+    await router.routingSync(req)
+    
