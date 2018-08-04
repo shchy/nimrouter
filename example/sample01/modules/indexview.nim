78ad662212d47f88138728d2ede69b745fff363b
shch <shch.pc@gmail.com>
2018-08-04 15:58:49 +0000

    save

--- example/sample01/modules/indexview.nim
+++ example/sample01/modules/indexview.nim
@@ -1,48 +1,42 @@
 import 
-    emerald,
     sequtils,
-    layoutview
+    layoutview,
+    alpaka/view
 
-proc signinViewTmpl() {.html_templ.} =
-    html(lang="en"):
-        call_mixin header("example"):
-            link(rel="stylesheet", href="/static/signin.css"): ""
-        body:
-            `div`(class="container"):
-                form(class="form-signin", action="/signin", `method`="POST"):
-                    h2(class="form-signin-heading"):"sign in"
-                    label(`for`="inputID", class="sr-only"):"Email address"
-                    input(`type`="text", id="inputID", name="id", class="form-control", placeholder="ID"):""
-                    label(`for`="inputPassword", class="sr-only"): "Password"
-                    input(`type`="password", id="inputPassword", name="password", class="form-control", placeholder="Password"):""
-                    # `div`(class="checkbox"):
-                    #     label:
-                    #         input(`type`="checkbox", name="isRemember", value="remember-me"): "Remember me"
-                    button(class="btn btn-lg btn-primary btn-block", `type`="submit"):"Sign in"
+# proc signinViewTmpl(): string =
+#     layout(
+#         "example01",
+
+#     )
+#     html(lang="en"):
+#         call_mixin header("example"):
+#             link(rel="stylesheet", href="/static/signin.css"): ""
+#         body:
+#             `div`(class="container"):
+#                 form(class="form-signin", action="/signin", `method`="POST"):
+#                     h2(class="form-signin-heading"):"sign in"
+#                     label(`for`="inputID", class="sr-only"):"Email address"
+#                     input(`type`="text", id="inputID", name="id", class="form-control", placeholder="ID"):""
+#                     label(`for`="inputPassword", class="sr-only"): "Password"
+#                     input(`type`="password", id="inputPassword", name="password", class="form-control", placeholder="Password"):""
+#                     # `div`(class="checkbox"):
+#                     #     label:
+#                     #         input(`type`="checkbox", name="isRemember", value="remember-me"): "Remember me"
+#                     button(class="btn btn-lg btn-primary btn-block", `type`="submit"):"Sign in"
   
-proc signinView*(): string =
-    var ss = newStringStream()
-    var tmpl = newsigninViewTmpl()
-    tmpl.render(ss)
-    ss.flush()
-    return ss.data
+# proc signinView*(): string =
+#     var ss = newStringStream()
+#     var tmpl = newsigninViewTmpl()
+#     tmpl.render(ss)
+#     ss.flush()
+#     return ss.data
         
-
-proc homeViewTmpl(name:string) {.html_templ.} =
-    html(lang="en"):
-        call_mixin header("example"):
-            ""
-        body:
-            `div`(class="container"):
-                h1(class=""):"Home"
-                label: name
-                
 proc homeView*(name:string): string =
-    var ss = newStringStream()
-    var tmpl = newhomeViewTmpl()
-    tmpl.name = name
-    tmpl.render(ss)
-    ss.flush()
-    return ss.data
+    layout("example01",
+        `div`(class="container",
+            h1(class="","Home"),
+            label(name)
+        )
+    )
         
             
--- example/sample01/modules/layoutview.nim
+++ example/sample01/modules/layoutview.nim
@@ -1,17 +1,23 @@
 import 
-    emerald,
-    sequtils
+    sequtils,
+    macros,
+    alpaka/view
 
-proc header*(title: string) {.html_mixin.} =
-    head:
-        meta(charset="utf-8"):""
-        meta(content="width=device-width,initial-scale=1", name="viewport"):""
-        title: title
+proc layout*(t: string, content: string): string =
+    return html(lang="en",
+        head(
+            meta(charset="utf-8"),
+            meta(content="width=device-width,initial-scale=1", name="viewport"),
+            title(t),
 
-        link(rel="stylesheet", href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"): ""
-        link(rel="stylesheet", href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap-theme.min.css"): ""
-        
-        script(`type`="text/javascript", src="https://code.jquery.com/jquery-3.3.1.slim.min.js"): ""
-        script(`type`="text/javascript", src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"): ""
-        script(`type`="text/javascript", src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"): ""
-        put mixin_content()
+            # link(rel="stylesheet", href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"),
+            # link(rel="stylesheet", href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap-theme.min.css"),
+            
+            # script(`type`="text/javascript", src="https://code.jquery.com/jquery-3.3.1.slim.min.js"),
+            # script(`type`="text/javascript", src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"),
+            # script(`type`="text/javascript", src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"),     
+        ),
+        body(
+            content
+        )
+    )
