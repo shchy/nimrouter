bbbbb28e27d8db7d4ac1473c344d210a19220584
shch <shch.pc@gmail.com>
2018-08-12 16:58:14 +0900

    save

---
+++ tests/testsessionauth.nim
@@ -0,0 +1,66 @@
+import 
+    unittest,
+    uri,
+    strutils,
+    tables,
+    sequtils,
+    base64,
+    testcommon,
+    ../src/alpaka,
+    ../src/alpaka/auth/sessionauth
+
+
+suite "test sessionauth":
+    let handler = choose(
+        GET >=> route("/") >=> text "hello",
+        GET >=> route("/auth/") >=> mustBeAuth >=> (handler(ctx) do: ctx.text "hello " & ctx.user.name),
+        GET >=> route("/signin") >=> (handler(ctx) do: 
+            if ctx.signin(ctx.req.getQueryParam "id",ctx.req.getQueryParam "pass"):
+                return ctx.redirect "/auth/"
+            return ctx.code Http401            
+            ),
+    )
+    let getUser = proc(id,pass: string): AuthedUser =
+        if id != "tester" or pass != "password":
+            return nil
+        return AuthedUser(
+            id: id,
+            name: id,
+            role: @["normal"]
+        )
+    let router = 
+        newRouter(handler)
+        .useSessionAuth(getUser, "/", "cookieName", "hashKey"
+        , 1000, "/", false, true)
+    test "sessionauth":
+        var context = router.routingTest(HttpGet, "/")
+        check(context.res.body == "hello")
+        check(context.res.code == Http200)
+        check(context.res.headers["content-type"] == "text/plain")
+        check(isNilOrWhitespace context.res.contentFilePath)
+
+        context = router.routingTest(HttpGet, "/auth/")
+        check(context.res.body == "")
+        check(context.res.code == Http302)
+        check(context.res.headers["Location"] == "/")
+        check(isNilOrWhitespace context.res.contentFilePath)
+        
+        context = router.routingTest(HttpGet, "/signin?id=aa&pass=bb")
+        check(context.res.body == "")
+        check(context.res.code == Http401)
+        check(isNilOrWhitespace context.res.contentFilePath)
+
+
+        context = router.routingTest(HttpGet, "/signin?id=tester&pass=password")
+        check(context.res.body == "")
+        check(context.res.code == Http302)
+        check(context.res.headers["Location"] == "/auth/")
+        check(isNilOrWhitespace context.res.contentFilePath)
+
+
+        context = router.routingTest(HttpGet, "/auth/")
+        check(context.res.body == "")
+        check(context.res.code == Http302)
+        check(context.res.headers["Location"] == "/")
+        check(isNilOrWhitespace context.res.contentFilePath)
+        
